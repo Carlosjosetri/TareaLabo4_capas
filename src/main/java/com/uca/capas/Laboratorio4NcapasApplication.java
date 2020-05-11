e9b47be19a35409b166fb1de215ef01866813a2e
carlosjosetri <00082217@uca.edu.sv>
2020-05-11 13:07:47 -0600

    casi terminado solo falta los html

---
+++ src/main/java/com/uca/capas/Laboratorio4NcapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio4NcapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio4NcapasApplication.class, args);
+	}
+
+}
